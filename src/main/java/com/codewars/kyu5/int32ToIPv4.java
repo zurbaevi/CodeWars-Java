2f83a55d437d1a1715dcbdd36874572512adbdb3
Nika <zurbaevi.nika@gmail.com>
2020-11-17 21:03:34 +0400

    Added solutions

--- src/main/java/com/codewars/kyu5/int32ToIPv4.java
+++ src/main/java/com/codewars/kyu5/int32ToIPv4.java
@@ -1,4 +1,13 @@
 package com.codewars.kyu5;
 
+/**
+ * @author Nika Zurbaevi
+ */
 public class int32ToIPv4 {
+    public static String longToIP(long ip) {
+        return ((ip >> 24) & 0xff) + "." +
+                ((ip >> 16) & 0xff) + "." +
+                ((ip >> 8) & 0xff) + "." +
+                (ip & 0xff);
+    }
 }
