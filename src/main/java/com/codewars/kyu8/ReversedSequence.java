2df8cd7d87ff88510c087d05bc45c86373fa14e9
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-01-03 20:37:30 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/ReversedSequence.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class ReversedSequence {
+    public static int[] reverse(int n) {
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = n - i;
+        }
+        return result;
+    }
+}
