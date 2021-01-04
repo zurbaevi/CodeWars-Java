7e038e313eaa2ecbee0e73880630c97c7867f7ec
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-01-04 16:48:56 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu7/MaxMinArrays.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu7;
+
+import static java.util.stream.IntStream.of;
+import static java.util.stream.IntStream.range;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class MaxMinArrays {
+    public static int[] solve(int[] arr) {
+        int[] sort = of(arr).sorted().toArray();
+        return range(0, arr.length).map(i -> sort[i % 2 > 0 ? i / 2 : sort.length - i / 2 - 1]).toArray();
+    }
+}
