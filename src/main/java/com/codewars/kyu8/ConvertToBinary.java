cb85e90580eabf98d6d9caf5407904e0dd76e348
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-01-05 00:51:32 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/ConvertToBinary.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class ConvertToBinary {
+    public static int toBinary(int n) {
+        return Integer.parseInt(Integer.toBinaryString(n));
+    }
+}
