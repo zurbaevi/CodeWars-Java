ed319721182966ae396bbaf3ca20bbcc73fa26b8
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-01-11 12:26:29 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu7/ShortestWord.java
@@ -0,0 +1,17 @@
+package com.codewars.kyu7;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class ShortestWord {
+    public static int findShort(String s) {
+        List<String> words = Arrays.asList(s.split(" "));
+        words = words.stream().sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+        return words.get(0).length();
+    }
+}
