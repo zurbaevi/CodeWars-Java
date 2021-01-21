6226b3dd6c816c4f9d3ae7a22a62466d9650eef5
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-01-21 20:43:25 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/RemoveStringSpaces.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class RemoveStringSpaces {
+    static String noSpace(final String x) {
+        return x.replace(" ", "");
+    }
+}
