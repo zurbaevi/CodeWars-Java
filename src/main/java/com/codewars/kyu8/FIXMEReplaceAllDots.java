042df64d747c04ccd92a4e1c0261f0fe211b366c
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-02-08 20:01:57 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/FIXMEReplaceAllDots.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class FIXMEReplaceAllDots {
+    public static String replaceDots(final String str) {
+        return str.replaceAll("\\.", "-");
+    }
+}
