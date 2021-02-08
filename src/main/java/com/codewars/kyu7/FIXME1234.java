5b72dc51e4d8ffca13389c809046b46660debfe9
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-02-08 20:33:57 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu7/FIXME1234.java
@@ -0,0 +1,34 @@
+package com.codewars.kyu7;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class FIXME1234 {
+    private static final Map<String, MyNumber> cache = new HashMap<>();
+
+    static MyNumber getNumber(final String name) {
+        return List.of(MyNumber.values()).get(("_oneichiun____twonideux____threesantroisfourshiquatre").indexOf(name) / 13);
+    }
+
+    static enum MyNumber {
+        ONE(1, "one", "ichi", "un"),
+        TWO(2, "two", "ni", "deux"),
+        THREE(3, "three", "san", "trois"),
+        FOUR(4, "four", "shi", "quatre");
+
+        private final int val;
+
+        private MyNumber(int val, String... names) {
+            this.val = val;
+            for (final String n : names) cache.put(n, this);
+        }
+
+        public int intValue() {
+            return this.val;
+        }
+    }
+}
