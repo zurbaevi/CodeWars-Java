f8dd4a71da903d815f26b4a5f56da5e2ebbd07b0
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-02-10 11:59:41 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu7/FIXMEHoursToSeconds.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu7;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class FIXMEHoursToSeconds {
+    public static long hoursToSeconds(final long hours) {
+        return 3600 * hours;
+    }
+}
