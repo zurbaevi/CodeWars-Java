77e43683d02bb70531628fcc8dfc19c9f323a029
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-02-25 19:25:09 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/GrasshopperSummation.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class GrasshopperSummation {
+    public static int summation(int n) {
+        return n * (n + 1) / 2;
+    }
+}
