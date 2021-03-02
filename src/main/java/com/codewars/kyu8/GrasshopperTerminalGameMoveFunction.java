2a72f05a3dcc055cc63cbb106e2ff8d12eb952f0
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-02 20:43:05 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/GrasshopperTerminalGameMoveFunction.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class GrasshopperTerminalGameMoveFunction {
+    public static int move(int position, int roll) {
+        return position + roll * 2;
+    }
+}
