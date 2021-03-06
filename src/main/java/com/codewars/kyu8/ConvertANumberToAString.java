268ced02d892a08a082da0fd5fc3181098c40fe4
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-06 19:53:23 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/ConvertANumberToAString.java
@@ -0,0 +1,10 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class ConvertANumberToAString {
+    public static String numberToString(int num) {
+        return String.valueOf(num);
+    }
+}
