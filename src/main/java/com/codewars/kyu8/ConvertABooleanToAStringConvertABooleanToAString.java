ff1f186a8c14b0af95a1b968519c55d9b568c212
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-09 00:10:59 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu8/ConvertABooleanToAStringConvertABooleanToAString.java
@@ -0,0 +1,14 @@
+package com.codewars.kyu8;
+
+/**
+ * @author Zurbaevi Nika
+ */
+public class ConvertABooleanToAStringConvertABooleanToAString {
+    public static String convert(boolean b){
+        if(b) {
+            return "true";
+        } else {
+            return "false";
+        }
+    }
+}
