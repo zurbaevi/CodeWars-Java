444d539d911443fa8c3865011be46ab9d064c323
Zurbaevi Nika <zurbaevi.nika@gmail.com>
2021-03-13 19:42:18 +0400

    Added solution

---
+++ src/main/java/com/codewars/kyu6/MultiplesOf3Or5.java
@@ -0,0 +1,13 @@
+package com.codewars.kyu6;
+
+public class MultiplesOf3Or5 {
+    public int solution(int number) {
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
