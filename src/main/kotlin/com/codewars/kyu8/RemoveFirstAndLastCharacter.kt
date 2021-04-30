05b778ef19c4417ac7324b290783b81443e5e2da
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-04-30 17:07:31 +0400

    Added solution

---
+++ src/main/kotlin/com/codewars/kyu8/RemoveFirstAndLastCharacter.kt
@@ -0,0 +1,8 @@
+package com.codewars.kyu8
+
+/**
+ * @author Zurbaevi Nika
+ */
+fun removeChar(str: String): String {
+    return str.substring(1, str.length - 1)
+}
--- src/main/kotlin/com/codewars/kyu8/ReturningStrings.kt
+++ src/main/kotlin/com/codewars/kyu8/ReturningStrings.kt
@@ -1,5 +1,8 @@
 package com.codewars.kyu8
 
+/**
+ * @author Zurbaevi Nika
+ */
 fun greet(name: String): String {
     return "Hello, $name how are you doing today?"
 }
