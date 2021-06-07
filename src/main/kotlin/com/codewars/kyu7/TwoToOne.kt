d0e0421cce1444cfcc6603274c5f3e4466ce6ef4
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-06-07 15:39:04 +0400

    Added solution

---
+++ src/main/kotlin/com/codewars/kyu7/TwoToOne.kt
@@ -0,0 +1,24 @@
+package com.codewars.kyu7
+
+fun longest(s1: String, s2: String): String {
+    val string = s1 + s2
+    val builder = StringBuilder()
+    var newString = ""
+    val stringList: ArrayList<String> = arrayListOf()
+
+    string.forEach {
+
+        if (!stringList.contains(it.toString())) {
+            stringList.add(it.toString())
+            stringList.sort()
+        }
+    }
+
+    stringList.forEach {
+        builder.append(it)
+    }
+
+    newString = builder.toString()
+
+    return newString
+}
