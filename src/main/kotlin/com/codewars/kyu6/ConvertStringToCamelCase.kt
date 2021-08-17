3142bbd544e2fdcf1459b1a6c65b825801b64538
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-17 20:32:11 +0400

    Added solution

---
+++ src/main/kotlin/com/codewars/kyu6/ConvertStringToCamelCase.kt
@@ -0,0 +1,11 @@
+package com.codewars.kyu6
+
+/**
+ * @author Zurbaevi Nika
+ */
+fun toCamelCase(str: String): String =
+    str.split("-", "_")
+        .mapIndexed { index, word ->
+            if (index != 0) word.capitalize() else word
+        }
+        .joinToString("")
