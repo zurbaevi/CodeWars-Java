f1a0d7078a5edbfa40252ade31da074ac67c6072
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-22 23:23:53 +0400

    Update ReversedWords.kt

--- src/main/kotlin/com/codewars/kyu8/ReversedWords.kt
+++ src/main/kotlin/com/codewars/kyu8/ReversedWords.kt
@@ -4,7 +4,5 @@ package com.codewars.kyu8
  * @author Zurbaevi Nika
  */
 object ReverseWords {
-    fun reverseWords(str: String): String {
-        return str.split(" ").reversed().joinToString(" ")
-    }
+    fun reverseWords(str: String): String = str.split(" ").reversed().joinToString(" ")
 }
