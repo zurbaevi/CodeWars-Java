65498942380c39fc4b3e7708146be1cd45b2f477
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-23 21:26:38 +0400

    Refactoring code CenturyFromYear.kt

--- src/main/kotlin/com/codewars/kyu8/CenturyFromYear.kt
+++ src/main/kotlin/com/codewars/kyu8/CenturyFromYear.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun century(number: Int): Int {
-    return if (number % 100 == 0) number / 100 else number / 100 + 1
-}
+fun century(number: Int): Int = if (number % 100 == 0) number / 100 else number / 100 + 1
