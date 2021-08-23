ed02d2f8770c193ffeec2e9253467ffa3ec61394
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-23 21:28:09 +0400

    Refactoring code IsNDivisibleByXAndY.kt

--- src/main/kotlin/com/codewars/kyu8/IsNDivisibleByXAndY.kt
+++ src/main/kotlin/com/codewars/kyu8/IsNDivisibleByXAndY.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun isDivisible(n: Int, x: Int, y: Int): Boolean {
-    return n % x == 0 && n % y == 0
-}
+fun isDivisible(n: Int, x: Int, y: Int): Boolean = n % x == 0 && n % y == 0
