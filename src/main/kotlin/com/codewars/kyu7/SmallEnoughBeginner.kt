f99940985c3911f7ff5d229f0005b09db671d4ee
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 16:44:59 +0400

    Update SmallEnoughBeginner.kt

--- src/main/kotlin/com/codewars/kyu7/SmallEnoughBeginner.kt
+++ src/main/kotlin/com/codewars/kyu7/SmallEnoughBeginner.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu7
 /**
  * @author Zurbaevi Nika
  */
-fun smallEnough(a: IntArray, limit: Int): Boolean {
-    return limit >= a.max()!!
-}
+fun smallEnough(a: IntArray, limit: Int): Boolean = limit >= a.max()!!
