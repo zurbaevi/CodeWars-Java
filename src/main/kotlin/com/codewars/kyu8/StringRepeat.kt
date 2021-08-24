211b046b5e9d0b9bb73d8bc55f7ce269dfb7cd1f
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:06:29 +0400

    Update StringRepeat.kt

--- src/main/kotlin/com/codewars/kyu8/StringRepeat.kt
+++ src/main/kotlin/com/codewars/kyu8/StringRepeat.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun repeatStr(r: Int, str: String): String {
-    return str.repeat(r)
-}
+fun repeatStr(r: Int, str: String): String = str.repeat(r)
