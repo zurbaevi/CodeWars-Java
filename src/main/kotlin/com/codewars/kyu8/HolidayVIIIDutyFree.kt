078e6dccb0fb1547b1055223e25d80adccc14d04
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:07:25 +0400

    Update HolidayVIIIDutyFree.kt

--- src/main/kotlin/com/codewars/kyu8/HolidayVIIIDutyFree.kt
+++ src/main/kotlin/com/codewars/kyu8/HolidayVIIIDutyFree.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun dutyFree(normPrice: Int, discount: Int, hol: Int): Int {
-    return hol * 100 / (normPrice * discount)
-}
+fun dutyFree(normPrice: Int, discount: Int, hol: Int): Int = hol * 100 / (normPrice * discount)
