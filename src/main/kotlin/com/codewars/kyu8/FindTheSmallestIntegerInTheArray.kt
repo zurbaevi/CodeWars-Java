6ddae648e4f63ade10e3e7f3a81148f7c98b30f5
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:07:40 +0400

    Update FindTheSmallestIntegerInTheArray.kt

--- src/main/kotlin/com/codewars/kyu8/FindTheSmallestIntegerInTheArray.kt
+++ src/main/kotlin/com/codewars/kyu8/FindTheSmallestIntegerInTheArray.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun findSmallestInt(nums: List<Int>): Int {
-    return nums.min()?.toInt() ?: 0
-}
+fun findSmallestInt(nums: List<Int>): Int = nums.min()?.toInt() ?: 0
