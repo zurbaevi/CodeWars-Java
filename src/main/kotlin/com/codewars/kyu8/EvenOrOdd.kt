9d7cae9bc6732fb2b45d84ab68d42b96246f32e9
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:06:10 +0400

    Update EvenOrOdd.kt

--- src/main/kotlin/com/codewars/kyu8/EvenOrOdd.kt
+++ src/main/kotlin/com/codewars/kyu8/EvenOrOdd.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun evenOrOdd(number: Int): String {
-    return if (number % 2 == 0) "Even" else "Odd"
-}
+fun evenOrOdd(number: Int): String = if (number % 2 == 0) "Even" else "Odd"
