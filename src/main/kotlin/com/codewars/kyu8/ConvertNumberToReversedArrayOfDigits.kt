7f8f9fa7cd3232a78304c39e11fe07e8ae1dde05
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:07:53 +0400

    Update ConvertNumberToReversedArrayOfDigits.kt

--- src/main/kotlin/com/codewars/kyu8/ConvertNumberToReversedArrayOfDigits.kt
+++ src/main/kotlin/com/codewars/kyu8/ConvertNumberToReversedArrayOfDigits.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun digitize(n: Long): IntArray {
-    return n.toString().reversed().map { it.toString().toInt() }.toIntArray()
-}
+fun digitize(n: Long): IntArray = n.toString().reversed().map { it.toString().toInt() }.toIntArray()
