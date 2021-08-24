d4f924233712367a168ad1b74f47c3f6c7039ba7
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-24 22:06:42 +0400

    Update TheFeastOfManyBeasts.kt

--- src/main/kotlin/com/codewars/kyu8/TheFeastOfManyBeasts.kt
+++ src/main/kotlin/com/codewars/kyu8/TheFeastOfManyBeasts.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun feast(beast: String, dish: String): Boolean {
-    return beast.first() == dish.first() && beast.last() == dish.last()
-}
+fun feast(beast: String, dish: String): Boolean = beast.first() == dish.first() && beast.last() == dish.last()
