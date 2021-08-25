c08427d3b151d57f9329ecef987eec140d80733b
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-25 18:53:54 +0400

    Update EvenNumbersInAnArray.kt

--- src/main/kotlin/com/codewars/kyu7/EvenNumbersInAnArray.kt
+++ src/main/kotlin/com/codewars/kyu7/EvenNumbersInAnArray.kt
@@ -3,6 +3,4 @@ package com.codewars.kyu7
 /**
  * @author Zurbaevi Nika
  */
-fun evenNumbers(array: List<Int>, number: Int): List<Int> {
-    return array.filter { it % 2 == 0 }.takeLast(number)
-}
+fun evenNumbers(array: List<Int>, number: Int): List<Int> = array.filter { it % 2 == 0 }.takeLast(number)
