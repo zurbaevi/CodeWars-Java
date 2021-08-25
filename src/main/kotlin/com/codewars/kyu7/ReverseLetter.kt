423931c178d7400512c1e189f0ae2a6cd3f98bb0
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-25 18:54:12 +0400

    Update ReverseLetter.kt

--- src/main/kotlin/com/codewars/kyu7/ReverseLetter.kt
+++ src/main/kotlin/com/codewars/kyu7/ReverseLetter.kt
@@ -1,5 +1,3 @@
 package com.codewars.kyu7
 
-fun reverseLetter(str: String): String {
-    return StringBuilder(str.replace("[^a-zA-Z]".toRegex(), "")).reverse().toString()
-}
+fun reverseLetter(str: String): String = StringBuilder(str.replace("[^a-zA-Z]".toRegex(), "")).reverse().toString()
