0b95498c45702acdb53906563ff67e0670bb51fd
Nika Zurbaevi <zurbaevi.nika@gmail.com>
2021-08-28 20:56:11 +0400

    Update SimpleMultiplication.kt

--- src/main/kotlin/com/codewars/kyu8/SimpleMultiplication.kt
+++ src/main/kotlin/com/codewars/kyu8/SimpleMultiplication.kt
@@ -3,8 +3,8 @@ package com.codewars.kyu8
 /**
  * @author Zurbaevi Nika
  */
-fun simpleMultiplication(n: Int): Int = if(n % 2 == 0){
-        n * 8
-    } else {
-        n * 9
-    }
+fun simpleMultiplication(n: Int): Int = if (n % 2 == 0) {
+    n * 8
+} else {
+    n * 9
+}
